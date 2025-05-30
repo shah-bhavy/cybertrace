624acda70c3d3459f625cdcc9a10f72f9fc7acfc
Bhavya Shah <109457225+shah-bhavy@users.noreply.github.com>
2025-05-30 23:44:46 +0530

    Update mock.php

--- api/mock.php
+++ api/mock.php
@@ -1,1 +1,9 @@
-
+<?php
+header('Content-Type: application/json');
+$devices = [
+    ['id' => 1, 'name' => 'Device 1', 'port' => 'USB Port 1'],
+    ['id' => 2, 'name' => 'Device 2', 'port' => 'USB Port 2'],
+    ['id' => 3, 'name' => 'External Storage', 'port' => 'USB Port 3']
+];
+echo json_encode($devices);
+?>
